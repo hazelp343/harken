a72f516e3b5438ae071a9af51b41a64c9dc4042d
杨明言 <292125771+hazelp343@users.noreply.github.com>
2026-06-28 12:10:43 +0800

    feat: export public api at package root

--- harken/__init__.py
+++ harken/__init__.py
@@ -1,5 +1,25 @@
 """harken: connect pretrained audio encoders to LLMs for audio question answering."""
 
 from harken._version import __version__
+from harken.audio_io import load_audio
+from harken.config import AudioQAConfig
+from harken.encoders import get_encoder, list_encoders
+from harken.modeling import AudioQAModel, build_model
+from harken.processor import AudioQAProcessor
+from harken.projectors import build_projector, list_projectors
+from harken.prompts import AUDIO_TOKEN, build_prompt
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "AUDIO_TOKEN",
+    "AudioQAConfig",
+    "AudioQAModel",
+    "AudioQAProcessor",
+    "build_model",
+    "build_prompt",
+    "build_projector",
+    "get_encoder",
+    "list_encoders",
+    "list_projectors",
+    "load_audio",
+]
